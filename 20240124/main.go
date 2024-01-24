31be8477f19226aa544f3941463cf1bfb838fa45
kusumoto-t <developer@DESKTOP-1JF67UJ.localdomain>
2024-01-24 21:40:28 +0900

    add 20240124

---
+++ 20240124/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/api/", apiHandler)
+
+	port := 8080
+	fmt.Printf("Starting Server at :%d", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+
+	if err != nil {
+		fmt.Println("Error Starting Server")
+	}
+
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "API endpoint")
+}
